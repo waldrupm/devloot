9b6913443f001527e521054af98865d4c5bb18c9
Micheal Waldrup <waldrupm@gmail.com>
2020-08-04 15:12:22 -0400

    add assets route

--- assets/views.py
+++ assets/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'assets/home.html')
+
+def single_asset(request):
+    return render(request, 'assets/single.html')
