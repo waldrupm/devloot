eeffacdda2638181687e2a3365cbcaa2c84c9cfb
Micheal Waldrup <waldrupm@gmail.com>
2020-08-06 18:48:57 -0400

    Add tags to assets

--- assets/models.py
+++ assets/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=30, null=True)
+    description = models.TextField(max_length=600, blank=True)
+
+    def __str__(self):
+        return f"Tag: {self.name}"
+
+
 class Asset(models.Model):
     TYPE_CHOICES = [
         ('3d', '3D'),
@@ -19,17 +27,10 @@ class Asset(models.Model):
     asset_type = models.CharField(max_length=25, choices=TYPE_CHOICES, null=True)
     creation_date = models.DateTimeField(auto_now_add=True, null=True)
     moved_to_general = models.DateTimeField(null=True, blank=True)
+    tags = models.ManyToManyField(Tag)
 
     class Meta:
         ordering = ('-creation_date', 'name')
 
     def __str__(self):
         return f"Asset: {self.name} - {self.id}"
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=30, null=True)
-    description = models.TextField(max_length=600, blank=True)
-
-    def __str__(self):
-        return f"Tag: {self.name}"
